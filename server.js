607c601dd4365b211e164b86509c1b284e021ce3
Aaron Jacobowitz <amjacobowitz@gmail.com>
2016-08-29 14:31:36 -0400

    add workers to possibly reduce memory usage

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length
+
+
+if (cluster.isMaster){
+	for (var i = 0; i < numCPUs; i++){
+		cluster.fork();
+	}
+
+	cluster.on('exit', function(worker, code, signal){
+		console.log('worker ' + worker.process.pid + ' died.' );
+	});
+} else {
 const path = require('path');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
@@ -163,3 +176,5 @@ app.listen(app.get('port'), () => {
 	console.log("Server listening on port " + app.get('port'));
 });
 
+
+}
