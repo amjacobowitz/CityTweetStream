035e216ed3ab28629fdf784416b16eb7db8cd361
Aaron Jacobowitz <amjacobowitz@gmail.com>
2016-08-29 14:36:50 -0400

    remove concurrency

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length
-
-
-if (cluster.isMaster){
-	for (var i = 0; i < numCPUs; i++){
-		cluster.fork();
-	}
-
-	cluster.on('exit', function(worker, code, signal){
-		console.log('worker ' + worker.process.pid + ' died.' );
-	});
-} else {
 const path = require('path');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
@@ -176,5 +163,3 @@ app.listen(app.get('port'), () => {
 	console.log("Server listening on port " + app.get('port'));
 });
 
-
-}
