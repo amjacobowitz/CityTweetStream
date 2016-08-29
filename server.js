add4672332deb8b14e026f765bf704c673596a38
Aaron Jacobowitz <amjacobowitz@gmail.com>
2016-08-29 13:23:15 -0400

    change syntax for port

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ var coordinates = sanFransiscoCoords
 	.concat(baltimoreCoords)
 	.concat(newOrleansCoords)
 
-server.listen(process.env.port || 8080);
+server.listen(8080);
 
 io.on('connection', function(socket){
 
@@ -144,7 +144,7 @@ io.on('connection', function(socket){
 });
 
 
-
+app.set('port', (process.env.PORT || 3000));
 
 
 app.use(express.static(__dirname + '/public'))
@@ -159,7 +159,7 @@ app.use(webpackDevMiddleware(compiler, {
 	noInfo: true
 }));
 
-app.listen(process.env.port || 3000, () => {
-	console.log("Server listening");
+app.listen(app.get('port'), () => {
+	console.log("Server listening on port " + app.get('port'));
 });
 
